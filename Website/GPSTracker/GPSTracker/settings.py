5ab29134eb41374aa1e853c3ea06f9afbcd1bf2f
Chris Ward <chris@cwardcode.com>
2013-12-25 02:33:23 -0500

    Fixed the server's config and deleted old django site proj.

--- Website/GPSTracker/GPSTracker/settings.py
+++ Website/GPSTracker/GPSTracker/settings.py
@@ -113,7 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/var/www/capstone/GPSTracker/templates",
+    "/var/www/capstone/Website/GPSTracker/templates",
 )
 
 INSTALLED_APPS = (
