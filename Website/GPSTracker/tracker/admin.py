10798c045e3404a99acea841ef3859e91371e286
Chris Ward <chris@cwardcode.com>
2014-01-04 16:50:15 -0500

    Added DB support for ridership info, updated chat client.

--- Website/GPSTracker/tracker/admin.py
+++ Website/GPSTracker/tracker/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from tracker.models import Location
 from tracker.models import Vehicle
+from tracker.models import PeopleCount
 
 admin.site.register(Location)
 admin.site.register(Vehicle)
+admin.site.register(PeopleCount)
--- Website/GPSTracker/tracker/models.py
+++ Website/GPSTracker/tracker/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Vehicle(models.Model):
-    VehID = models.AutoField(primary_key=True)
-    Title = models.CharField(max_length=40)
+    VehID  = models.AutoField(primary_key=True)
+    Title  = models.CharField(max_length=40)
+    Driver = models.CharField(max_length=25)
 
     def __unicode__(self):
         return self.Title
@@ -18,3 +19,16 @@ class Location(models.Model):
         #VehID + LocID Identifier
         return str(self.VehID) + "/"+ str(self.LocID)
 
+
+class PeopleCount(models.Model):
+    CountID = models.AutoField(primary_key=True)
+    VehID = models.ForeignKey('Vehicle')
+    LocID = models.ForeignKey('Location')
+    Date  = models.DateField(auto_now_add=True, blank=False)
+    Date.editable=True
+    Time  = models.TimeField(auto_now_add=True)
+    Time.editable=True
+    Count = models.IntegerField()
+
+    def __unicode__(self):
+        return str(self.CountID)
