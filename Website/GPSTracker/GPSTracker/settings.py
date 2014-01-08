68f9750a6a677ad8e39c804ce88dd516a8c6f4dd
Chris Ward <chris@cwardcode.com>
2014-01-08 04:21:51 -0500

    Added chartit to project

--- Website/GPSTracker/GPSTracker/settings.py
+++ Website/GPSTracker/GPSTracker/settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'tracker',
+    'chartit',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- Website/GPSTracker/tracker/views.py
+++ Website/GPSTracker/tracker/views.py
@@ -1,7 +1,8 @@
+from chartit import DataPool, Chart
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader, RequestContext
-
+from tracker.models import PeopleCount
 def home(request):
     return render_to_response('index.html')
 
@@ -10,6 +11,37 @@ def test(request):
 
 def chat(request):
     return render_to_response('chat.html')
+
+def chart(request):
+    ridershipData = \
+            DataPool(
+                    series=
+                    [{'options': {
+                        'source': PeopleCount.objects.all()},
+                        'terms': [
+                            'Count',
+                            'StopID',
+                            'LocID']}
+                        ])
+    riderChart = Chart(
+            datasource = ridershipData,
+            series_options = 
+            [{'options':{
+                'type': 'line',
+                'stacking': False},
+                'terms':{
+                    'LocID': [
+                        'Count',
+                        'StopID']
+                    }}],
+             chart_options = 
+             {'title': {
+                 'text': 'Ridership Data'},
+                 'xAxis':{
+                     'title': {
+                         'text': 'Rider Count'}}})
+
+    return render_to_response('chart.html',{'ridershipChart':riderChart})
 
 def help(request):
     return render_to_response('help.html')
