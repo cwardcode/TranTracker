9e75ceba95c6a6e9baeaf582cddccbfe81e5a502
Chris Ward <chris@cwardcode.com>
2014-03-13 05:37:29 -0400

    Added chart for change_list page, and functions to get totals by stopIDs

--- Website/GPSTracker/tracker/models.py
+++ Website/GPSTracker/tracker/models.py
@@ -33,25 +33,38 @@ class PeopleCount(models.Model):
 
     Date.editable = True
     Time.editable = True
-    def totalPeople():
+
+    def totalPeople(self):
         totPeople = 0
-        for model in PeopleCount:
+        for model in PeopleCount.objects.all():
             totPeople += model.Count
-        return tot_people
+        return totPeople
 
     def __unicode__(self):
         return str(self.CountID)
 
     def peoplecount_chart(self):
-        #        lu = { 'categories' : self.StopID.objects.all(),\
-                #     'count' : self.Count,}
+
+       totPeople = self.totalPeople()
 
        lu = { 'categories' : [self.StopID],\
                'tot_riders' : [self.Count],\
-               'tot_riders_at_stop' : [200]} 
+               'tot_riders_at_stop' : [totPeople]} 
 
        return render_to_string('admin/tracker/peoplecount/peoplecount_chart.html', lu )
     peoplecount_chart.allow_tags = True
+
+    def allstops_chart(self):
+
+       totPeople = self.totalPeople()
+
+       lu = { 'categories' : [self.StopID],\
+               'tot_riders' : [self.Count],\
+               'tot_riders_at_stop' : [totPeople]} 
+
+       return render_to_string('admin/tracker/peoplecount/allstops_chart.html', lu )
+    allstops_chart.allow_tags = True
+
 
 class StopLocation(models.Model):
     StopID = models.AutoField(primary_key=True)
