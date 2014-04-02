5c5d868f93d36d262d8a2f703d6a8224a743c5a8
Chris Ward <chris@cwardcode.com>
2014-04-02 06:45:14 -0400

    Added iOS project, google maps API implemented.

---
+++ TrackerApps/TrackerClients/iOS/TrackeriOSApp/TrackerAppTests/TrackerAppTests.m
@@ -0,0 +1,34 @@
+//
+//  TrackerAppTests.m
+//  TrackerAppTests
+//
+//  Created by Chris Ward on 4/2/14.
+//  Copyright (c) 2014 Chris Ward. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TrackerAppTests : XCTestCase
+
+@end
+
+@implementation TrackerAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
