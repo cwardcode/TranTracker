1683624f32af4aa6c6c05c75bf6f6b860fef1d59
Chris Ward <chris@cwardcode.com>
2014-06-02 01:20:26 -0400

    TrackerApp now displays speeds in MPH, rather than m/s

--- TrackerApps/TrackerClients/Android/TrackerAndroidApp/src/edu/wcu/trackerapp/StopList.java
+++ TrackerApps/TrackerClients/Android/TrackerAndroidApp/src/edu/wcu/trackerapp/StopList.java
@@ -1,13 +1,10 @@
 package edu.wcu.trackerapp;
 
-import edu.wcu.trackerapp.AppConstants;
-import edu.wcu.trackerapp.StopSelection;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
